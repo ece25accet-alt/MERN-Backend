64434b934986ade3fddc064948a8ac34654f8729
ece25accet-alt <ece25accet@gmail.com>
2026-06-02 12:58:26 +0530

    Add files via upload

---
+++ MERN-Backend-3-5/js/day1/assets/task1.js
@@ -0,0 +1,13 @@
+
+var alpha = 1;
+let beta = 2;
+const pi = 3.1452;
+
+alpha = 2;
+var alpha = 4; //Var can be re-declared and re-assigned
+beta = 5; //let variable can be re assigned but it won't be re-declared
+
+//const is a constant so it will neither be re-declarred nor be re-assigned
+console.log(alpha);
+console.log(beta);
+console.log(pi);
